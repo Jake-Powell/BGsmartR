75fe1ac1807bf30ec9c65e70ca86aa868d8f76c7
Jake Powell <jjp68@cam.ac.uk>
2023-09-21 13:28:46 +0100

    add flexability to enrich_collection(), + document

--- R/enrich_report.R
+++ R/enrich_report.R
@@ -1,19 +1,21 @@
 #' Enrich a collection's database
 #'
-#' This function enriches plat records in a collection using information from POWO (WCVP), IUCN redlist and BGCI.
+#' This function enriches plant records in a collection using information from POWO (WCVP), IUCN redlist and BGCI.
 #'
 #'
 #' @param collection A data frame containing a collection.
-#' @param wcvp World Checklist of Vascular Plants (WCVP) database, obtained using the function [import_wcvp_names()].
-#' @param redList RedList database
-#' @param BGCI Requires a cleaned BGCI plant search database to obtain how many collections globally a taxon is contained (Not freely available).
+#' @param wcvp World Checklist of Vascular Plants (WCVP) database, obtained using the function [import_wcvp_names()]. If `NA` WCVP enrichment is not performed.
+#' @param redList RedList database. If `NA` IUCN red list enrichment is not performed.
+#' @param BGCI Requires a cleaned BGCI plant search database to obtain how many collections globally a taxon is contained (Not freely available). If `NA` BGCI plant search enrichment is not performed.
 #' @param taxon_name_column The name of the column in the `collection` corresponding to taxonomic names.
 #' @param taxon_name_full_column The name of the column in the `collection` corresponding to joined taxonomic names and authors.
 #' @param taxon_author_column The name of the column in the `collection` corresponding to the authors of the taxonomic names.
 #' @param do_is_autonym Flag (TRUE/FALSE) for whether to add the column is_autonym, see [add_is_autonym()] for details.
 #' @param do_status_year Flag (TRUE/FALSE) for whether to add the column status_year, see [add_status_year()] for details.
 #' @param do_taxon_types Flag (TRUE/FALSE) for whether to add the column taxon_type, see [add_taxon_type()] for details.
-#' @param typo_method Defines the method used to find typos in the taxonomic name. Allowed values are `"full"`,  `"fast"`, or `"no"`. These correspond to full search (see [match_typos()]), search only using [typo_list] and no typo searching.
+#' @param ... Arguments (i.e., attributes) used in the matching algorithm (passed along to [match_original_to_wcvp()]). Examples include `typo_method`, `do_convert_accepted` and `try_fix_hybrid`.
+#' @param wcvp_wanted_info A character vector containing the information we want to extract from WCVP. These are matched to column names in `wcvp$wcvp_names`, `wcvp$geography` and `wcvp$redlist` (if the later two exist).
+#' @param redlist_wanted_info A character vector containing the information we want to extract from IUCN red list, corresponding to column names in `redList`
 #'
 #' @return The `collection` data frame enriched with information dependent on function inputs (new columns).
 #' @export
@@ -24,7 +26,13 @@ enrich_collection <- function(collection,
                           taxon_name_column = 'TaxonName',
                           taxon_name_full_column = NA,
                           taxon_author_column = NA,
-                          do_is_autonym = FALSE, do_status_year = FALSE, do_taxon_types = FALSE, typo_method = 'fast'){
+                          do_is_autonym = FALSE,
+                          do_status_year = FALSE,
+                          do_taxon_types = FALSE,
+                          ...,
+                          wcvp_wanted_info = c("plant_name_id", "taxon_name", "taxon_authors", "taxon_rank", "taxon_status","powo_id", "family", "genus", "species", "lifeform_description", "climate_description", "geographic_area", "Dist_000_area_code_l3", "Dist_000_labels", "Dist_100_area_code_l3", "Dist_010_area_code_l3", "Dist_001_area_code_l3", "Dist_101_area_code_l3", "Dist_110_area_code_l3", "Dist_011_plant_name_id", "Dist_011_area_code_l3", "main_common_name", "assessment_date", "category", "criteria", "population_trend"),
+                          redlist_wanted_info = c("plant_name_id", "main_common_name", "assessment_date", "category",           "criteria", "population_trend")
+                          ){
 
   ############################################
   # 1) Clean/extract taxon name + taxon author
@@ -72,49 +80,88 @@ enrich_collection <- function(collection,
                                       taxon_name_col = 'sanitised_taxon',
                                       taxon_name_full_col = NA,
                                       taxon_author_col = 'extracted_author',
-                                      typo_method = typo_method)
+                                      ...)
+  enriched_report = data.frame(enriched_report,
+                               POWO_original_authors = match_info$original_authors,
+                               POWO_match_taxon_name = match_info$match_taxon_name,
+                               POWO_match_authors = match_info$match_authors,
+                               POWO_author_check = match_info$author_check,
+                               POWO_match_detail = match_info$details,
+                               POWO_match_detail_short =match_info$details_short)
 
-  # B) Extract the info from wcvp_names.
-  wcvp_wanted_columns = c("plant_name_id", "taxon_name", "taxon_authors", "taxon_rank", "taxon_status","powo_id", "family", "genus", "species", "lifeform_description", "climate_description", "geographic_area")
+
+  # B) Force plant_name_id to be in wanted info as it's needed for geography and red list info.
+  wcvp_wanted_info_orig = wcvp_wanted_info
+  wcvp_wanted_info = unique(c('plant_name_id', wcvp_wanted_info))
+
+  # C) Extract the info from wcvp_names.
+  wcvp_wanted_columns = wcvp_wanted_info
   wcvp_wanted_columns = wcvp_wanted_columns[wcvp_wanted_columns %in% names(wcvp$wcvp_names)]
   POWO_info = data.frame(matrix(NA, nrow = nrow(collection), ncol = length(wcvp_wanted_columns)))
   names(POWO_info) = paste0('POWO_',wcvp_wanted_columns)
   indices = which(!(is.na(match_info$match) | match_info$match < 0))
   POWO_info[indices,] = wcvp$wcvp_names[match_info$match[indices],match(wcvp_wanted_columns,names(wcvp$wcvp_names))]
+  if('plant_name_id' %in% wcvp_wanted_info_orig){
+    enriched_report = data.frame(enriched_report, POWO_info)
+  }else{
+    enriched_report = data.frame(enriched_report, POWO_info[,-1])  #plant_name_id always the first column.
+  }
 
-  # C) Add info from wcvp_distributions.
-  Geog_info = data.frame(matrix(NA, nrow = nrow(collection), ncol = length(names(wcvp$geography))))
-  havePOWO_index = which(!is.na(POWO_info$POWO_plant_name_id))
-  plant_id_match = match(POWO_info$POWO_plant_name_id[havePOWO_index], wcvp$geography$plant_name_id)
+  # D) Add info from wcvp_distributions.
+  if('geography' %in% names(wcvp)){
+    wcvp_wanted_columns = wcvp_wanted_info
+    wcvp_wanted_columns = wcvp_wanted_columns[wcvp_wanted_columns %in% names(wcvp$geography)]
+    if(length(wcvp_wanted_columns)>1){
+      Geog_info = data.frame(matrix(NA, nrow = nrow(collection), ncol = length(wcvp_wanted_columns)))
+      havePOWO_index = which(!is.na(POWO_info$POWO_plant_name_id))
+      plant_id_match = match(POWO_info$POWO_plant_name_id[havePOWO_index], wcvp$geography$plant_name_id)
 
-  Geog_info[havePOWO_index,] = wcvp$geography[plant_id_match,]
-  names(Geog_info) = paste0('POWO_', names(wcvp$geography))
+      Geog_info[havePOWO_index,] = wcvp$geography[plant_id_match,match(wcvp_wanted_columns,names(wcvp$geography))]
+      names(Geog_info) = paste0('POWO_', wcvp_wanted_columns)
+      Geog_info = Geog_info[,-1] # remove plant_name_id column.
 
-  # D) Add info from WCVP matched to Redlist.
-  wanted_columns = c("plant_name_id", "main_common_name", "assessment_date", "category",           "criteria", "population_trend")
-  red_data = wcvp$redList[,match(wanted_columns, names(wcvp$redList))]
-  Red_info = data.frame(matrix(NA, nrow = nrow(collection), ncol = length(wanted_columns)))
-  havePOWO_index = which(!is.na(POWO_info$POWO_plant_name_id))
-  plant_id_match = match(POWO_info$POWO_plant_name_id[havePOWO_index], red_data$plant_name_id)
+      enriched_report = data.frame(enriched_report, Geog_info)
 
-  Red_info[havePOWO_index,] = red_data[plant_id_match,]
-  names(Red_info) = paste0('POWO_Red_',wanted_columns)
+      if(length(wcvp_wanted_columns)==2){
+        names(enriched_report)[length(names(enriched_report))] = paste0('POWO_',wcvp_wanted_columns[2])
+      }
+    }
 
-  # E) Create POWO web address.
-  POWO_web_address = rep(NA, nrow(POWO_info))
-  indices = !is.na(POWO_info$POWO_powo_id)
-  POWO_web_address[indices] = paste0('https://powo.science.kew.org/taxon/urn:lsid:ipni.org:names:',POWO_info$POWO_powo_id[indices])
+  }
+
+  # E) Add info from WCVP matched to Redlist.
+  if('redList' %in% names(wcvp)){
+    wcvp_wanted_columns = wcvp_wanted_info
+    wcvp_wanted_columns = wcvp_wanted_columns[wcvp_wanted_columns %in% names(wcvp$redList)]
+    if(length(wcvp_wanted_columns)>1){
+      Red_info = data.frame(matrix(NA, nrow = nrow(collection), ncol = length(wcvp_wanted_columns)))
+      havePOWO_index = which(!is.na(POWO_info$POWO_plant_name_id))
+      plant_id_match = match(POWO_info$POWO_plant_name_id[havePOWO_index], wcvp$redList$plant_name_id)
+
+      Red_info[havePOWO_index,] = wcvp$redList[plant_id_match,match(wcvp_wanted_columns,names(wcvp$redList))]
+      names(Red_info) = paste0('POWO_Red_',wcvp_wanted_columns)
+      Red_info = Red_info[,-1] # remove plant_name_id column.
+
+      enriched_report = data.frame(enriched_report, Red_info)
+
+      if(length(wcvp_wanted_columns)==2){
+        names(enriched_report)[length(names(enriched_report))] = paste0('POWO_Red_',wcvp_wanted_columns[2])
+      }
+
+    }
 
 
-  # E) add to enriched report.
-  enriched_report = data.frame(enriched_report,
-                               original_authors = match_info$original_authors,
-                               match_taxon_name = match_info$match_taxon_name,
-                               match_authors = match_info$match_authors,
-                               author_check = match_info$author_check,
-                               match_detail = match_info$details,
-                               match_detail_short =match_info$details_short,
-                               POWO_web_address = POWO_web_address, POWO_info, Geog_info[,-1], Red_info[,-1])
+  }
+
+  # F) Create POWO web address.
+  if('powo_id' %in% wcvp_wanted_info){
+    POWO_web_address = rep(NA, nrow(POWO_info))
+    indices = !is.na(POWO_info$POWO_powo_id)
+    POWO_web_address[indices] = paste0('https://powo.science.kew.org/taxon/urn:lsid:ipni.org:names:',POWO_info$POWO_powo_id[indices])
+
+    enriched_report = data.frame(enriched_report, POWO_web_address = POWO_web_address)
+
+  }
   }
 
   ############################################
@@ -141,13 +188,9 @@ enrich_collection <- function(collection,
                                         taxon_name_col = 'sanitised_taxon',
                                         taxon_name_full_col = NA,
                                         taxon_author_col = 'extracted_author',
-                                        typo_method = typo_method)
+                                        ...)
 
-    redList_wanted_columns = c("scientific_name", 'authority', "main_common_name",
-                               "category", "published_year", "assessment_date",
-                               'criteria', 'population_trend',
-                               'aoo_km2', 'eoo_km2',
-                               'elevation_upper', 'elevation_lower')
+    redList_wanted_columns = redlist_wanted_info
     redList_wanted_columns = redList_wanted_columns[redList_wanted_columns %in% names(redList)]
     redList_info = data.frame(matrix(NA, nrow = nrow(collection), ncol = length(redList_wanted_columns)))
     names(redList_info) = paste0('redList_',redList_wanted_columns)
